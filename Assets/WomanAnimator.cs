f05249562e7f92e7b43831997b9958c45209e0bd
vicfabrice <discosdevictoria@gmail.com>
2017-09-20 11:32:23 -0300

    uploading folders

---
+++ Assets/WomanAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class WomanAnimator : MonoBehaviour {
+
+    public Sprite[] sprites;
+    public float framesPerSecond;
+    private SpriteRenderer spriteRenderer;
+    
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+        index = index % sprites.Length;
+        spriteRenderer.sprite = sprites[index];
+
+
+    }
+}
